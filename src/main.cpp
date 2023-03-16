1d8808bb367f17c3082efa71e6d58e8a3a0b6580
Krokovlad <vladik.channel17@gmail.com>
2023-03-16 23:54:12 +0300

    Added base logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "SSID сети";
+const char* password = "Пароль";
+const char* serverName = "Адрес по которому будет отправлен запрос";
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 30000;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Timer set to 30 seconds");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if ((millis() - lastTime) > timerDelay) {
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      http.begin(client, serverName);
+      http.addHeader("Content-Type", "text/plain");
+      //String httpRequestData = "test";
+      int httpResponseCode = http.POST("RFID");
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+} 
